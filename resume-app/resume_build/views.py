24f35d476b8d9a047cd5333360a3f8407bfa4909
Atyender <atyenderpalsingh@gmail.com>
2024-11-12 16:44:04 -0500

    create resume form

--- resume-app/resume_build/views.py
+++ resume-app/resume_build/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ResumeForm
 
-# Create your views here.
+def create_resume(request):
+    if request.method == 'POST':
+        form = ResumeForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the data to the database
+            return redirect('resume_success')  # Redirect after saving
+    else:
+        form = ResumeForm()
+
+    return render(request, 'resume_build/create_resume.html', {'form': form})
